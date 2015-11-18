535c280164f315af2b34e2c0135b5f903f7a1eaa
Bruno Palos <bruno.palos@mindera.com>
2015-11-18 16:19:51 +0000

    Adding random response generator.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var restify = require('restify');
+var random = require('randomstring');
+var errors = require('restify-errors');
 
 // Default values
 var defaultPort = 8080;
 var defaultMaxMillis = 300000;
 
-function respond(req, res, next) {
+var maxByteSize = 1048576;
+var megaByteString = random.generate(maxByteSize);
+
+function respondMimic(req, res, next) {
     var millis = parseInt(req.params.millis);
     var response = req.params.response;
 
@@ -18,8 +23,55 @@ function respond(req, res, next) {
     next();
 }
 
+function respondRandom(req, res, next) {
+    var millis = parseInt(req.params.millis);
+    var size = req.params.size;
+
+    if (!isMillisValid(millis) || !isSizeValid(size)) {
+        res.send(400);
+    } else {
+        setTimeout(function () {
+            var bytes = convertToBytes(size);
+            res.send(megaByteString.substring(0, bytes));
+        }, millis);
+    }
+    next();
+}
+
 function isMillisValid(millis) {
     return !isNaN(millis) && millis < defaultMaxMillis;
+}
+
+function isSizeValid(size) {
+    if (parseInt(size) > maxByteSize) {
+        return false;
+    } else {
+        return /(1M)|(1000K)|(^\d{1,3}K?$)|\d+/.test(size);
+    }
+}
+
+function convertToBytes(size) {
+    var multiplier = getMultiplier(size);
+    var value = parseInt(size.match(/\d+/));
+
+    switch (multiplier) {
+        case 'K':
+            return value * 1024;
+            break;
+        case 'M':
+            return value * maxByteSize;
+            break;
+        default:
+            return value;
+    }
+}
+
+function getMultiplier(size) {
+    var multiplier = size.match(/[KM]/);
+    if (multiplier !== null) {
+        return multiplier[0];
+    }
+    return '';
 }
 
 function isResponseValid(response) {
@@ -36,7 +88,8 @@ function getPortParam() {
 }
 
 var server = restify.createServer();
-server.get('/slowdown/:millis/:response', respond);
+server.get('/slowdown/:millis/:response', respondMimic);
+server.get('/slowdown-random/:millis/:size', respondRandom);
 
 server.listen(getPortParam(), function() {
     console.log('%s listening at %s', server.name, server.url);
